9f8988f410af2ba0b0393d6afde158d12237bebb
SafeHavenFinance <85708635+SafeHavenFinance@users.noreply.github.com>
2022-07-20 18:01:26 +0200

    Update farms.js

--- src/constants/farms.js
+++ src/constants/farms.js
@@ -8,22 +8,22 @@ const farms = [
     lpSymbol: "SLT-WBNB LP",
     lpAddresses: {
       97: "",
-      56: "0x41A7B4f3BF16B710f1Ba14c51c32Fc0D6e6Bab2d",
+      56: "0xd45ca0E8a8d977D906D1959D3B0DBab298aB41EC",
     },
     token: serializedTokens.slt,
     quoteToken: serializedTokens.wbnb,
-    strat: "0xB05141717A188547429B909f70B327CB44D841a0",
+    strat: "0xb7ab5Eb548fd978d90CAF23d48069E541877584E",
   },
   {
     pid: 1,
     lpSymbol: "SLT-BUSD LP",
     lpAddresses: {
       97: "",
-      56: "0x5852C4fabeCf79FCfEA8F5559019c086C08B1CEB",
+      56: "0xBdEe59544B4EcDB326Be39B309f8Ab2E1c98777C",
     },
     token: serializedTokens.slt,
     quoteToken: serializedTokens.busd,
-    strat: "0xd495097b5fD3CB1b98D9Cf16fa32bD20dBe05cc9",
+    strat: "0x421A3d59610270F32aB5042AA0B6c94Bc6a0238D",
   },
   {
     pid: 2,
@@ -34,7 +34,7 @@ const farms = [
     },
     token: serializedTokens.wbnb,
     quoteToken: serializedTokens.busd,
-    strat: "0x826e77074eD5E5d11d185CA601Afd0af38Cc0a4F",
+    strat: "0xe9aC111B9cc68E4FbC5a92B1781cB40Ba66F4d1c",
   },
   {
     pid: 3,
@@ -45,7 +45,7 @@ const farms = [
     },
     token: serializedTokens.usdt,
     quoteToken: serializedTokens.busd,
-    strat: "0x913cA633a3bFFBa9d1265c263597e8d2D612812D",
+    strat: "0x9875bB549013Ae4fA7d774c484A2158944724BBb",
   },
   {
     pid: 4,
@@ -56,7 +56,7 @@ const farms = [
     },
     token: serializedTokens.usdc,
     quoteToken: serializedTokens.busd,
-    strat: "0x07B1e283D3904DB44B08db0Fd5fe27f17F6E60D3",
+    strat: "0x613586Cd289aC05653f0CF239D945E8ca1561E2d",
   },
   {
     pid: 5,
@@ -67,40 +67,7 @@ const farms = [
     },
     token: serializedTokens.cake,
     quoteToken: serializedTokens.wbnb,
-    strat: "0xCC5572e251b4BB27dcF6802F42B70842Ff19Bf17",
-  },
-  {
-    pid: 10,
-    lpSymbol: "WBNB-BUSD LP",
-    lpAddresses: {
-      97: "",
-      56: "0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16",
-    },
-    token: serializedTokens.wbnb,
-    quoteToken: serializedTokens.busd,
-    strat: "0x25f1d637E43C6995ce39bB0fc6Ce942cd618c681",
-  },
-  {
-    pid: 11,
-    lpSymbol: "USDT-BUSD LP",
-    lpAddresses: {
-      97: "",
-      56: "0x7EFaEf62fDdCCa950418312c6C91Aef321375A00",
-    },
-    token: serializedTokens.usdt,
-    quoteToken: serializedTokens.busd,
-    strat: "0xaA853dbBb4c99b5f63ec71eF265d0D78f26698B7",
-  },
-  {
-    pid: 12,
-    lpSymbol: "CAKE-WBNB LP",
-    lpAddresses: {
-      97: "",
-      56: "0x0eD7e52944161450477ee417DE9Cd3a859b14fD0",
-    },
-    token: serializedTokens.cake,
-    quoteToken: serializedTokens.wbnb,
-    strat: "0x9305AbA88c44286f7E7B545e2a94D08544e8F342",
+    strat: "0xF64c6AD16B08696366b72cA4638aA3d89A7A7e65",
   },
 ];
 
