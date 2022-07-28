efb9e570f8f4510760dd04965c6407c7d52fb811
SafeHavenFinance <85708635+SafeHavenFinance@users.noreply.github.com>
2022-07-28 11:23:52 +0200

    Delete fetchPool.js

--- src/store/slices/pools-slice/fetchPool.js
+++
@@ -1,9 +0,0 @@
-import fetchPublicStakeData from "./fetchPublicPoolData";
-
-const fetchPool = async (stake) => {
-  const stakePublicData = await fetchPublicStakeData(stake);
-  // console.log("stake-stakePublicData", stake, stakePublicData)
-  return { ...stake, ...stakePublicData };
-};
-
-export default fetchPool;
